118824174876c47692ff4b168ced27b822081747
catherinekwan0521-dotcom <catherinekwan.0521@gmail.com>
2026-03-22 19:05:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-//
+// 我的token
 const token = '8721828503:AAH-fdnsPJkjTeDKqd9oj4UHvXQOMjaJeRc';
 
 const bot = new TelegramBot(token, { polling: true });
 
-// 模拟数据库
 let users = {};
 
 // /start
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
 
-    // 如果用户不存在 → 创建
     if (!users[chatId]) {
         const memberId = "MY" + Math.floor(100000 + Math.random() * 900000);
 
@@ -20,7 +18,8 @@ bot.onText(/\/start/, (msg) => {
             telegram_id: chatId,
             member_id: memberId,
             phone: null,
-            balance: 0
+            balance: 0,
+            language: null
         };
     }
 
@@ -45,38 +44,46 @@ Please select language:`,
         }
     );
 });
+
+// 处理按钮
 bot.on("callback_query", (query) => {
     const chatId = query.message.chat.id;
     const data = query.data;
 
-    // 保存用户语言
+    // 语言选择
     if (data === "en" || data === "bm" || data === "cn") {
 
-    users[chatId].language = data;
+        users[chatId].language = data;
 
-    let message = "";
-    let buttonText = "";
+        let message = "";
+        let buttonText = "";
 
-    if (data === "en") {
-        message = "🎁 Welcome Bonus RM10\n\nClick register 👇";
-        buttonText = "👉 Register";
-    }
-
-    if (data === "bm") {
-        message = "🎁 Bonus Selamat Datang RM10\n\nKlik daftar 👇";
-        buttonText = "👉 Daftar";
-    }
-
-    if (data === "cn") {
-        message = "🎁 欢迎奖金 RM10\n\n点击注册 👇";
-        buttonText = "👉 注册";
-    }
-
-    bot.sendMessage(chatId, message, {
-        reply_markup: {
-            inline_keyboard: [
-                [{ text: buttonText, callback_data: "register" }]
-            ]
+        if (data === "en") {
+            message = "🎁 Welcome Bonus RM10\n\nClick register 👇";
+            buttonText = "👉 Register";
         }
-    });
-}
+
+        if (data === "bm") {
+            message = "🎁 Bonus Selamat Datang RM10\n\nKlik daftar 👇";
+            buttonText = "👉 Daftar";
+        }
+
+        if (data === "cn") {
+            message = "🎁 欢迎奖金 RM10\n\n点击注册 👇";
+            buttonText = "👉 注册";
+        }
+
+        bot.sendMessage(chatId, message, {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: buttonText, callback_data: "register" }]
+                ]
+            }
+        });
+    }
+
+    // register按钮（先留空）
+    if (data === "register") {
+        bot.sendMessage(chatId, "📱 Please share your phone:");
+    }
+});
